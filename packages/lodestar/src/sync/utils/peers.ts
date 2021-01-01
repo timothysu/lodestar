a059eec5445647f8e121cbdbc312758ff69db9d7
dapplion <guillem.lion.benosa@gmail.com>
2021-01-01 21:54:13 +0100

    Use consistent style in getSyncPeers

--- packages/lodestar/src/sync/utils/peers.ts
+++ packages/lodestar/src/sync/utils/peers.ts
@@ -9,20 +9,13 @@ export function getSyncPeers(
   maxPeers = 10,
   minScore = 60
 ): PeerId[] {
-  return (
-    network
-      .getPeers({
-        connected: true,
-        supportsProtocols: getSyncProtocols(),
-      })
-      .map((peer) => peer.id)
-      .filter((peer) => {
-        return network.peerRpcScores.getScore(peer) > minScore && condition(peer);
-      })
-      .sort((p1, p2) => {
-        return network.peerRpcScores.getScore(p2) - network.peerRpcScores.getScore(p1);
-      })
-      // take 10 best peers for sync
-      .slice(0, maxPeers)
-  );
+  return network
+    .getPeers({
+      connected: true,
+      supportsProtocols: getSyncProtocols(),
+    })
+    .map((peer) => peer.id)
+    .filter((peer) => network.peerRpcScores.getScore(peer) > minScore && condition(peer))
+    .sort((p1, p2) => network.peerRpcScores.getScore(p2) - network.peerRpcScores.getScore(p1))
+    .slice(0, maxPeers);
 }
