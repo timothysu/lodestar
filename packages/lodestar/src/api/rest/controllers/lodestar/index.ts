e82dabee919d2598dc5410f01c4931985f4ec991
dapplion <35266934+dapplion@users.noreply.github.com>
2021-03-01 18:17:26 +0100

    API heapdump

--- packages/lodestar/src/api/rest/controllers/lodestar/index.ts
+++ packages/lodestar/src/api/rest/controllers/lodestar/index.ts
@@ -8,3 +8,17 @@ export const getWtfNode: ApiController<DefaultQuery> = {
   },
   opts: {},
 };
+
+export const getHeapdump: ApiController<DefaultQuery> = {
+  url: "/write-heapdump/",
+  handler: function (req, resp) {
+    // eslint-disable-next-line
+    const heapdump = require("heapdump");
+    const file = `lodestar-beacon-${new Date().toISOString()}.heapsnapshot`;
+    heapdump.writeSnapshot(file, function (err: Error, filename: string) {
+      if (err) resp.status(500).send(err);
+      else resp.status(200).send(`Wrote heapdump to ${filename}`);
+    });
+  },
+  opts: {},
+};
--- packages/lodestar/src/api/rest/routes/lodestar.ts
+++ packages/lodestar/src/api/rest/routes/lodestar.ts
@@ -1,5 +1,5 @@
 import {FastifyInstance} from "fastify";
-import {getWtfNode} from "../controllers/lodestar";
+import {getWtfNode, getHeapdump} from "../controllers/lodestar";
 
 /**
  * Register /lodestar route
@@ -7,6 +7,7 @@ import {getWtfNode} from "../controllers/lodestar";
 export function registerLodestarRoutes(server: FastifyInstance): void {
   server.register(
     async function (fastify) {
+      fastify.get(getHeapdump.url, getHeapdump.opts, getHeapdump.handler);
       fastify.get(getWtfNode.url, getWtfNode.opts, getWtfNode.handler);
     },
     {prefix: "/v1/lodestar"}
