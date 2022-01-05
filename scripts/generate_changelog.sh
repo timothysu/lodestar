69d9ead9b2588b8bb2ebb38a4d060c314fab0b75
dapplion <35266934+dapplion@users.noreply.github.com>
2022-01-05 11:51:22 +0100

    Forward token

--- scripts/generate_changelog.sh
+++ scripts/generate_changelog.sh
@@ -1,6 +1,13 @@
 # Must set ENV CHANGELOG_GITHUB_TOKEN to the Github token
 
-docker run --rm -v "$(pwd)":/usr/local/src/your-app githubchangeloggenerator/github-changelog-generator \
+CHANGELOG_OUTPUT_PATH=./CHANGELOG.md
+
+docker run --rm \
+  # Must be run in lodestar's directory
+  -v "$(pwd)":/usr/local/src/your-app \
+  # Forward the value of the ENV CHANGELOG_GITHUB_TOKEN without displaying it
+  -e CHANGELOG_GITHUB_TOKEN \
+  githubchangeloggenerator/github-changelog-generator \
   --user chainsafe \
   --project lodestar \
   --issues false \
@@ -11,5 +18,10 @@ docker run --rm -v "$(pwd)":/usr/local/src/your-app githubchangeloggenerator/git
   --filter-by-milestone false \
   --unreleased false \
   --exclude-labels meta-excludefromchangelog \
+  # TODO: Set as argument
   --since-tag v0.32.0 \
-  --output ./CHANGELOG.md
+  --output $CHANGELOG_OUTPUT_PATH
+
+# Strip generator notice.
+echo "Stripping generator notice."
+sed -i '/This Changelog was automatically generated/d' "$CHANGELOG_OUTPUT_PATH"
